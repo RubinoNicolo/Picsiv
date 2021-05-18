97e46c268e4c788e8c30389a6db208c9d1144a26
Umberto Barascu <73645183+BarascuRazvan@users.noreply.github.com>
2021-05-18 09:54:36 +0200

    Update Album.java

--- Picasso/Codice/Album.java
+++ Picasso/Codice/Album.java
@@ -1,13 +1,10 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Album {
-    
     private List<Foto> album;
-    
     public ListaAmici(){
-        this.album= new ArrayList<>();
+        this.album = new ArrayList<>();
     }
     public void addFoto(Foto a){
         album.add(a);
@@ -21,5 +18,5 @@ public class Album {
     public void addTag(int num, Utenti[] tag){
      Foto editata = new Foto(album[num]);
      editata.setTag(tag);
-     album[num]=editata;
+     album[num] = editata;
     }
