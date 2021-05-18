7cf22e84dca3d258956c5080319eee9f4f08f231
BarascuRazvan <73645183+BarascuRazvan@users.noreply.github.com>
2021-05-18 09:33:27 +0200

    Create Album.java
    #Creata classe album con metodi addFoto,removeFoto,addTag

---
+++ Picasso/Album.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    
+    private List<Foto> album;
+    
+    public ListaAmici(){
+        this.album= new ArrayList<>();
+    }
+    public void addFoto(Foto a){
+        album.add(a);
+    }
+    public void removeFoto(Foto a){
+        album.remove(a);
+    }
+    public void getFoto(int num){
+      album.get(num);
+    }
+    public void addTag(int num, Utenti[] tag){
+     Foto editata = new Foto(album[num]);
+     editata.setTag(tag);
+     album[num]=editata;
+    }
