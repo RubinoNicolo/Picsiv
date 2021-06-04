50fef7d233b23a689c01168126b2f0bd0a5a6d57
Umberto Barascu <73645183+BarascuRazvan@users.noreply.github.com>
2021-06-04 15:52:55 +0200

    Aggiunto addUtente, getCont

--- Picasso/Codice/Foto.java
+++ Picasso/Codice/Foto.java
@@ -5,24 +5,28 @@ public class Foto{
     private String risoluzione;
     private String descrizione;
     private Utente[] tag;
+    private int cont;
     private String path;
     public Foto(){
         risoluzione = "";
         descrizione = "";
         tag = new Utente[20];
         path = "";
+        cont = 0;
     }
     public Foto(String risoluzione,String descrizione,Utente[] tag,String path){
         this.risoluzione = risoluzione;
         this.descrizione = descrizione;
         this.tag = tag;
         this.path = path;
+        this.cont = 0;
     }
     public Foto(Foto f){
         this.risoluzione = f.risoluzione;
         this.descrizione = f.descrizione;
         this.tag = f.tag;
         this.path = f.path;
+        this.cont= f.cont;
     }
     public void setRisoluzione(String risoluzione){
         this.risoluzione = risoluzione;
@@ -47,6 +51,18 @@ public class Foto{
     }
     public Utente[] getTag () {
         return tag;   
+    }
+    public void addUtente(Utente a){
+        if (cont<tag.length){
+            tag[cont]=utente;
+            }
+    }
+    public int getCont(Utente [] tags){
+    int contatore = 0;
+    for(i=0;(i<tags.length)&&(tags[i]!=null)<;i++){
+        contatore = contatore + 1;
+    }
+    return contatore;
     }
     public int getNumeroAmici(){
         int cont = 0;
